9fe0f4081f21b6c8348a6bba4f451e5d85435b11
Timo Behrmann <timo.behrmann@gmail.com>
2014-05-23 13:37:44 +0200

    modal

--- app/scripts/apps/default/DefaultApp.js
+++ app/scripts/apps/default/DefaultApp.js
@@ -1,7 +1,8 @@
-define(['angular'], function (angular) {
+define(['angular', 'angular-bootstrap-ui-transition', 'angular-bootstrap-ui-modal'], function (angular) {
     'use strict';
 
     angular.module('DefaultApp', [
+        'ui.bootstrap.modal',
         /*angJSDeps*/
         'ngCookies',
         'ngResource',
@@ -18,8 +19,45 @@ define(['angular'], function (angular) {
                     controller: 'DefaultHelloCtrl'
                 })
         })
-        .controller('DefaultStartCtrl', function () {})
+        .controller('DefaultStartCtrl', function ($scope, $modal) {
+            $scope.items = ['item1', 'item2', 'item3'];
+
+            $scope.openModal = function($event) {
+                $event.preventDefault();
+
+                var modalInstance = $modal.open({
+                    templateUrl: 'scripts/apps/default/templates/modal.tpl.html',
+                    controller: 'ModalInstanceCtrl',
+                    resolve: {
+                        items: function () {
+                            return $scope.items;
+                        }
+                    }
+                });
+
+                modalInstance.result.then(function (selectedItem) {
+                    $scope.selected = selectedItem;
+                }, function () {
+                    $log.info('Modal dismissed at: ' + new Date());
+                });
+
+            };
+        })
         .controller('DefaultHelloCtrl', function ($scope, $routeParams) {
             $scope.name = $routeParams.name;
+        })
+        .controller('ModalInstanceCtrl', function ($scope, $modalInstance, items) {
+            $scope.items = items;
+            $scope.selected = {
+                item: $scope.items[0]
+            };
+
+            $scope.ok = function () {
+                $modalInstance.close($scope.selected.item);
+            };
+
+            $scope.cancel = function () {
+                $modalInstance.dismiss('cancel');
+            };
         });
 });
